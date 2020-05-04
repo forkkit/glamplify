0903008aab18a538b036c55d6ad7e310185caca6
techmanmike <michael.ridgway@gmail.com>
2020-05-04 15:30:52 +1000

    Removed comment from test

--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -35,7 +35,6 @@ func Test_JWT_Encode(t *testing.T) {
 	assert.Assert(t, header == "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9", header)
 
 	fmt.Print(token)
-
 	// Note: Hard to test payload/signature because there is a elapsed time field that always changes...
 }
 
@@ -52,8 +51,6 @@ func Test_JWT_Decode(t *testing.T) {
 
 	jwt, err := NewDecoderFromPath("jwt.rs256.key.development.pub")
 	assert.Assert(t, err == nil, err)
-
-	// token := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50SWQiOiJhYmMxMjMiLCJlZmZlY3RpdmVVc2VySWQiOiJ4eXozNDUiLCJyZWFsVXNlcklkIjoieHl6MjM0IiwiZXhwIjoxNTg4NTY4OTY4LCJpYXQiOjE1ODg1NjgzNjh9.bQKvgB8ZfGSZDQnyfM1remW_cB_sF95iIS-QfJmdn3jOCK60xiwMB7cNkXBLeVsCyMScHiTyENePvCOudsMruNhWO8YvnBpv6712O4n2sRckTKaNdYXAwidILDFXzRZMzrFAOJu1zKjPSaXiXdEv6zgq3OMruXcBF9RzsOlKPoOlgBI1Q9ctGurgI-p4WovCA0YmV9w7I2c1t3WQMMlapoJPKW1-bM37sgEgJpJrmjCavYswQ_mWY0yk9h8ftXGvQRPvLXM_K-kkhmUJ1cLT-H4iXIZkCk-Y-ONAej9lPOgBGiCmOq5DHHcggOKzzqcT0YNKrZHfCrigd7ZbT-zRSw9ukzYafduabCSj9MAq_oKzYYbYpqu6yNtzHXFBZ7izWjGVMUxpQX5gaFh6W0aezWwmBL6drO1NzYDSMX2lJ-FwVCVfKbvqFPxS5mqYQCAQueGrTlrIndWqVdDbJFw2LHTFxVLAFQGgnM292WJYp6KYVKm07mRpzHdozb8ER7lfB_hlloudEBh14WxnV4iKZabjsGZmpzXldSdVKceXTBSY4jobE-vD_U2YfBcNU7y_A2qJtTnhdIWMq_UZrmi5ycV_Nq5MUSjLat-J8iFSkCeOEsyxQ3ybMiaxFEbpBZoZAIrRUJxx3KWtdzywyS4dN9frv36O0UuWomskaR1il6U"
 
 	payload, err := jwt.Decode(token)
 
